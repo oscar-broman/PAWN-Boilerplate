2605026cf3ccb4e769b4cf5fcf0a0237dac9ede8
Oscar Broman <oscar.broman@gmail.com>
2012-04-14 15:24:57 +0200

    the compiler will now download amx_assembly if needed

--- compiler/pre-compiler/compile.php
+++ compiler/pre-compiler/compile.php
@@ -132,6 +132,49 @@ if (!file_exists('samp-server.exe') || !file_exists('include/a_samp.inc')) {
 	echo "\nDone.\n";
 }
 
+if (!file_exists('include/amx_assembly/amx_header.inc')) {
+	echo "Downloading amx_assembly, please wait.\n";
+	
+	$tmpfile = tempnam(sys_get_temp_dir(), 'amx_assembly.zip');
+	
+	file_put_contents($tmpfile, @file_get_contents('https://github.com/Zeex/amx_assembly/zipball/master'));
+	
+	if (filesize($tmpfile) == 0) {
+		echo "PBP Error: Unable to download amx_assembly from https://github.com/Zeex/amx_assembly/zipball/master\n";
+		
+		exit;
+	}
+	
+	echo "Downloaded.\n";
+	
+	$zip = new ZipArchive;
+	$res = $zip->open($tmpfile);
+	
+	if ($res === true) {
+		$zip->extractTo('include/amx_assembly');
+		
+		$folder = strstr($zip->getNameIndex(0), '/', true);
+		
+		foreach (glob("include/amx_assembly/$folder/*") as $file) {
+			$basename = basename($file);
+			$newfile = "include/amx_assembly/$basename";
+			
+			if (file_exists($newfile))
+				unlink($newfile);
+			
+			rename($file, $newfile);
+		}
+		
+		$zip->close();
+	} else {
+		echo "Unable to open the zip archive.\n";
+		
+		exit;
+	}
+	
+	echo "\nDone.\n";
+}
+
 require 'class.PBP.php';
 
 $pbp = new PBP();
